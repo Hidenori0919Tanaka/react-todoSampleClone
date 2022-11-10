e8b25400a7b78a3d5afedd251c3efebe1c658278
TNK_Hidy <hidenori091985@yahoo.co.jp>
2022-11-11 08:20:26 +0900

    add form

--- react-practice/src/components/TodoList.js
+++ react-practice/src/components/TodoList.js
@@ -7,14 +7,33 @@ const Todo = () => {
             isCompleted: false
         }
     ];
-    const [text, setText] = useState(initialState);
+    const [todos, setTodos] = useState(initialState);
+    const [task, setTask] = useState('')
+    const handleOnSubmit = (event) => {
+        event.preventDefault()
+        if (task === '') return
+        setTodos(todos => [...todos, { task, isCompleted: false }])
+        setTask('')
+    }
+    const handleNewTask = (event) => {
+        setTask(event.target.value)
+    }
 
     return (
         <div>
             <h1>ToDo List</h1>
+            <form onSubmit={handleOnSubmit}>
+                Add Task :
+                <input
+                    value={task}
+                    placeholder="Add New Task"
+                    onChange={handleNewTask}
+                />
+                <button type="submit">Add</button>
+            </form>
             <ul>
-                {text.map((todo, index) => (
-                    text.length > 1 && text.task !== '' && text.isCompleted ?
+                {todos.map((todo, index) => (
+                    todos.length > 1 && todos.task !== '' && todos.isCompleted ?
                         <li key={index}>{todo.task}</li> : <li></li>
                 ))}
             </ul>
